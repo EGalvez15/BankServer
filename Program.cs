bf91b50ec16c463f9b4a937180d8af8939e27733
Erick Galvez <erickgalvez54@yahoo.com>
2022-01-18 17:26:38 -0500

    start creation of bankserver app

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Npgsql;
+
+const string cs = "Server=localhost;Port=5432;Password=hentai42069;Username=postgres;";
+
+using var con = new NpgsqlConnection(cs);
+con.Open();
+
+var sql = "SELECT version()";
+
+using var cmd = new NpgsqlCommand(sql, con);
+
+var version = cmd.ExecuteScalar().ToString();
+Console.WriteLine($"PostgreSQL version: {version}");
+
+con.Close();
